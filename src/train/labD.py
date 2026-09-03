40ec96dd4637f918fa08f23ac2842d1325452371
misang-30 <coco7428@naver.com>
2026-09-03 16:34:10 +0900

    ...

--- src/train/labD.py
+++ src/train/labD.py
@@ -1,8 +1,34 @@
 from labC import smallcnn
+import torch
+import torch.nn as nn
+from torchvision import models
 
 
-def restnetScrat() :
-    # 
+def restnet18() :
+
+    model = models.resnet18(weights=None)
+
+    model.conv1 = nn.Conv2d(
+        3, 64,
+        kernel_size=3,
+        stride=1,
+        padding=1,
+        bias=False
+    )
+
+    model.maxpool = nn.Identity()
+
+    model.fc = nn.Linear(model.fc.in_features, 10)
+    # CIFAR-10 32×32
+    #     ↓
+    # 3×3 Conv
+    #     ↓
+    # ResNet18
+    #     ↓
+    # FC
+    #     ↓
+    # 10 classes
+
 
 def resnetPre() :
     # 
