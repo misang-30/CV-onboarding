f7d0ac448b7d5e99efd4d219aacf4178d58a5795
misang-30 <coco7428@naver.com>
2026-09-03 16:20:25 +0900

    lab D

---
+++ src/train/labD.py
@@ -0,0 +1,23 @@
+from labC import smallcnn
+
+
+def restnetScrat() :
+    # 
+
+def resnetPre() :
+    # 
+
+
+
+if __name__ == "__main__":
+    print("<< Direct Call >> \n")
+    print("<< Day 5 : LabC >> \n")
+
+    print("Handmade SmallCNN ")
+    smallcnn()
+
+    print("Resnet - Scratch Version")
+    #
+
+    print("Resnet - Pretrained")
+    #
